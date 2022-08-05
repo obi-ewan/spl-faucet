42afeaf7009bed2b418b17fcb9a29cb4bd058997
obi-ewan <ewan@betdex.com>
2022-08-05 11:37:44 +0100

    tests

--- programs/betdex-spl-faucet/src/lib.rs
+++ programs/betdex-spl-faucet/src/lib.rs
@@ -10,11 +10,18 @@ pub mod betdex_spl_faucet {
     use super::*;
     use anchor_spl::token;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_faucet(ctx: Context<Initialize>, airdrop_amount: u64, token_limit: u64) -> Result<()> {
+        let config = &mut ctx.accounts.config;
+        config.amount = airdrop_amount;
+        config.limit = token_limit;
         Ok(())
     }
 
     pub fn airdrop(ctx: Context<Airdrop>) -> Result<()> {
+        require!(
+            ctx.accounts.payer_token_account.amount < ctx.accounts.config.limit,
+            FaucetError::WalletLimitReached
+        );
 
         token::transfer(
             CpiContext::new_with_signer(
@@ -24,9 +31,12 @@ pub mod betdex_spl_faucet {
                     to: ctx.accounts.payer_token_account.to_account_info(),
                     authority: ctx.accounts.token_vault.to_account_info(),
                 },
-                &[&[ctx.accounts.mint.key().as_ref()]],
+                &[&[
+                    "faucet_token_vault".as_ref(), ctx.accounts.mint.key().as_ref(),
+                    &[*ctx.bumps.get("token_vault").unwrap()]
+                ]],
             ),
-            (100 * 10_u32.pow(ctx.accounts.mint.decimals as u32)) as u64,
+            ctx.accounts.config.amount,
         )
     }
 }
@@ -34,14 +44,22 @@ pub mod betdex_spl_faucet {
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(
-    init,
-    seeds=[mint.key().as_ref()],
-    bump,
-    payer=payer,
-    token::mint=mint,
-    token::authority=token_vault
+        init,
+        seeds=[b"faucet_token_vault".as_ref(), mint.key().as_ref()],
+        bump,
+        payer=payer,
+        token::mint=mint,
+        token::authority=token_vault
     )]
     pub token_vault: Account<'info, TokenAccount>,
+    #[account(
+        init,
+        seeds=[b"faucet_config".as_ref(), mint.key().as_ref()],
+        bump,
+        payer=payer,
+        space= 8 + 8 + 8
+    )]
+    pub config: Account<'info, FaucetConfig>,
     #[account(mut)]
     pub payer: Signer<'info>,
     pub mint: Account<'info, Mint>,
@@ -59,15 +77,32 @@ pub struct Airdrop<'info> {
     #[account(mut)]
     pub payer_token_account: Account<'info, TokenAccount>,
     #[account(
-    mut,
-    seeds=[mint.key().as_ref()],
-    bump,
-    token::mint=mint,
-    token::authority=token_vault
+        mut,
+        seeds=[b"faucet_token_vault".as_ref(), mint.key().as_ref()],
+        bump,
+        token::mint=mint,
+        token::authority=token_vault
     )]
     pub token_vault: Account<'info, TokenAccount>,
+    #[account(
+        seeds=[b"faucet_config".as_ref(), mint.key().as_ref()],
+        bump
+    )]
+    pub config: Account<'info, FaucetConfig>,
     pub mint: Account<'info, Mint>,
 
     #[account(address = anchor_spl::token::ID)]
     pub token_program: Program<'info, Token>,
+}
+
+#[account]
+pub struct FaucetConfig {
+    pub amount: u64,    // number of tokens airdropped
+    pub limit: u64      // max number of tokens a wallet can have present
+}
+
+#[error_code]
+pub enum FaucetError {
+    #[msg("Wallet has reached token limit for this faucet.")]
+    WalletLimitReached
 }
--- tests/betdex-spl-faucet.ts
+++ tests/betdex-spl-faucet.ts
@@ -1,16 +1,126 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { BetdexSplFaucet } from "../target/types/betdex_spl_faucet";
+import {AnchorProvider, BN, Program, Provider} from "@project-serum/anchor";
+import {BetdexSplFaucet} from "../target/types/betdex_spl_faucet";
+import {PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY} from "@solana/web3.js";
+import NodeWallet from "@project-serum/anchor/dist/esm/nodewallet";
+import {Token, TOKEN_PROGRAM_ID} from "@solana/spl-token";
+import * as assert from "assert";
 
 describe("betdex-spl-faucet", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.BetdexSplFaucet as Program<BetdexSplFaucet>;
+    const program = anchor.workspace.BetdexSplFaucet as Program<BetdexSplFaucet>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    it("Initialize new faucet", async () => {
+      const faucet = await initializeFaucet(program, provider)
+
+        const createdConfig = await program.account.faucetConfig.fetch(faucet.faucetConfigPk);
+        assert.equal(createdConfig.amount.toNumber(),100 * 10 ** 9);
+        assert.equal(createdConfig.limit.toNumber(), 1000 * 10 ** 9);
+    });
+
+    it('Airdrop tokens', async function () {
+        const faucet = await initializeFaucet(program, provider)
+
+        const mint = faucet.mint;
+        const tokenAccount = await mint.createAssociatedTokenAccount(provider.wallet.publicKey)
+
+        await mint.mintTo(
+            faucet.tokenVaultPk,
+            provider.wallet.publicKey,
+            [],
+            100 * 10 ** 9,
+        );
+
+        await program.methods.airdrop().accounts({
+            payer: provider.wallet.publicKey,
+            payerTokenAccount: tokenAccount,
+            tokenVault: faucet.tokenVaultPk,
+            config: faucet.faucetConfigPk,
+            mint: faucet.mint.publicKey,
+            tokenProgram: TOKEN_PROGRAM_ID,
+        }).rpc();
+
+
+        const tokenBalance = await provider.connection.getTokenAccountBalance(tokenAccount);
+        assert.equal(tokenBalance.value.amount, 100 * 10 ** 9);
+    });
+
+    it('Airdrop tokens - hits limit', async function () {
+        const faucet = await initializeFaucet(program, provider)
+
+        const mint = faucet.mint;
+        const tokenAccount = await mint.createAssociatedTokenAccount(provider.wallet.publicKey)
+
+        await mint.mintTo(
+            faucet.tokenVaultPk,
+            provider.wallet.publicKey,
+            [],
+            100 * 10 ** 9,
+        );
+        await mint.mintTo(
+            tokenAccount,
+            provider.wallet.publicKey,
+            [],
+            1000 * 10 ** 9,
+        );
+
+        try {
+            await program.methods.airdrop().accounts({
+                payer: provider.wallet.publicKey,
+                payerTokenAccount: tokenAccount,
+                tokenVault: faucet.tokenVaultPk,
+                config: faucet.faucetConfigPk,
+                mint: faucet.mint.publicKey,
+                tokenProgram: TOKEN_PROGRAM_ID,
+            }).rpc();
+        } catch (e) {
+            assert.equal(e.error.errorCode.code, "WalletLimitReached")
+        }
+
+        const tokenBalance = await provider.connection.getTokenAccountBalance(tokenAccount);
+        assert.equal(tokenBalance.value.amount, 1000 * 10 ** 9);
+    });
 });
+
+
+async function initializeFaucet(program: Program<BetdexSplFaucet>, provider: AnchorProvider) {
+    const mintDecimals = 9;
+    const airdropAmount = 100_000_000_000; // 100;
+    const limit = 1000_000_000_000; // 1000;
+
+    const wallet = provider.wallet as NodeWallet;
+    const mint = await Token.createMint(
+        provider.connection,
+        wallet.payer,
+        wallet.publicKey,
+        wallet.publicKey,
+        mintDecimals,
+        TOKEN_PROGRAM_ID,
+    );
+
+    const [tokenVaultPk] = await PublicKey.findProgramAddress(
+        [Buffer.from("faucet_token_vault"), mint.publicKey.toBuffer()], program.programId,
+    );
+
+    const [faucetConfigPk] = await PublicKey.findProgramAddress(
+        [Buffer.from("faucet_config"), mint.publicKey.toBuffer()], program.programId,
+    );
+
+    await program
+        .methods
+        .initializeFaucet(new BN(airdropAmount), new BN(limit))
+        .accounts({
+            tokenVault: tokenVaultPk,
+            config: faucetConfigPk,
+            payer: provider.wallet.publicKey,
+            mint: mint.publicKey,
+            rent: SYSVAR_RENT_PUBKEY,
+            systemProgram: SystemProgram.programId,
+            tokenProgram: TOKEN_PROGRAM_ID,
+        }).rpc();
+
+    return {tokenVaultPk, faucetConfigPk, mint}
+}
